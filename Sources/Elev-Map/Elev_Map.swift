83ee03772f7c76d8de4b30b88b00732a4088b612
CJKorn <cjkornie@gmail.com>
2026-08-29 17:44:24 +1000

    Initial Initial commit

---
+++ Sources/Elev-Map/Elev_Map.swift
@@ -0,0 +1,13 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+@main
+struct Elev_Map {
+    static func main() {
+        print("Hello, world!")
+    }
+}
+
+
+// Swift for API access and parsing for GLO-30 and DEM-S-30
+// Send to webUI using js and stuff for displaying 3D map using most likely threeJS
